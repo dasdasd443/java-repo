f2c886b7b89b6903172448fbeb39e6f5600a5a52
dasdasd443 <cjvicro@gmail.com>
2021-03-25 06:38:14 -0700

    Added for loops

--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class main {
     public static void main(String args[]) {
         int choice = 1;
@@ -10,6 +10,35 @@ public class main {
         boolean d;
         char xcd = '\0';
 
-        
+        asd = choice;
+        System.out.println(asd);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your choice: ");
+        choice = input.nextInt();
+
+        if(choice == 1){
+            System.out.println("You have choicesn 1");
+        }
+
+        int i = 0;
+        for (;i<5;i++){
+            System.out.println(i);
+        }
+
+        while(i >= 0){
+            System.out.println("-"+ i--);
+        }
+
+        do{
+            System.out.println("+"+ i++);
+        }while(i<5);
+
+        int array[] = {1,2,3,4,5};
+        for(int anothernumber:array)
+        {
+            System.out.println(anothernumber);
+        }
     }
 }
