8148257f234b22b7ac93f6511f5a1523d705cb66
dasdasd443 <cjvicro@gmail.com>
2021-03-29 06:09:16 -0700

    Conditional statements (if and switch)

--- src/main.java
+++ src/main.java
@@ -30,6 +30,36 @@ public class main {
             default: System.out.println("This is not a command!");break;
         }
 
+        System.out.println("Enter the day: ");
+        int day = input.nextInt();
+
+        if(day == 1){
+            System.out.println("Monday");
+        }else if(day == 2){
+            System.out.println("Tuesday");
+        }else if(day == 3){
+            System.out.println("Wednesday");
+        }else if(day == 4){
+            System.out.println("Thursday");
+        }else if(day == 5){
+            System.out.println("Friday");
+        }else if(day == 6){
+            System.out.println("Saturday");
+        }else if(day == 7){
+            System.out.println("Sunday");
+        }
+
+        switch(day){
+            case 1: System.out.println("Monday");break;
+            case 2: System.out.println("Tuesday");break;
+            case 3: System.out.println("Wednesday");break;
+            case 4: System.out.println("Thursday");break;
+            case 5: System.out.println("Friday");break;
+            case 6: System.out.println("Saturday");break;
+            case 7: System.out.println("Sunday");break;
+        }
+
+
 
     }
 
